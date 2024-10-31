28b874cd7879cc5bd00327dfe41a4c1851c29c09
harwarl <harwarl87@gmail.com>
2024-10-31 23:40:16 +0100

    commands setup

---
+++ src/config/config.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { DISCORD_CLIENT_ID, DISCORD_TOKEN } = process.env;
+
+if (!DISCORD_CLIENT_ID || !DISCORD_TOKEN) {
+  throw new Error("Missing Environment Variables");
+}
+
+export const config = {
+  DISCORD_CLIENT_ID,
+  DISCORD_TOKEN,
+};
